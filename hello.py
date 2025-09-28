28ebe8d778e35cb05d187498b132751ab90bd19b
A-yanfe-23 <olaoreabiodun521@gmail.com>
2025-09-28 18:25:24 +0100

    renamed flask_app.py to hello.py
    added base.html to template

--- hello.py
+++ hello.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('user.html', username=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
